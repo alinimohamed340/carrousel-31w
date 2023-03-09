8eb44c9980811d8d297cb299df4407baa37ba429
alini mohamed <alinimohamed340@gmail.com>
2023-03-09 14:51:13 -0500

    cours 12 carrousel.php enqueue  et generation du code html

--- sass/carrousel.php
+++ sass/carrousel.php
@@ -16,6 +16,11 @@
  */
 
 
+ function carrousel_enqueue(){
+
+$version_css = filemtime(plugin_dir_path(__FILE__ ) . "style.css");
+$version_js = filemtime(plugin_dir_path(__FILE__) . "js/carrousel.js");
+ 
  wp_enqueue_style(   'em_plugin_carrousel_css',
                      plugin_dir_url(__FILE__) . "style.css",
                      array(),
@@ -27,5 +32,24 @@
                     $version_js,
                     true);
 
+ }
+
+ add_action('wp_enqueue_scripts', 'carrousel_enqueue');
+
+
+
+ // la partie php qui va generer le formulaire et le contenu du carrousel
+
+ function creation_carrousel()
+ {
+    return '<button class="bouton__ouvrir">Ouvrir</button>
+    <div class="carrousel">
+    <button class="bouton__x">X</button>
+    <figure class="carrousel__figure"></figure>
+    <form class="carrousel__form"></form>
+    </div>';
+ }
+
+ add_shortcode('carrousel', 'creation_carrousel');
 
 ?>
